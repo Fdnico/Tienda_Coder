d4f29cc9072ad3fb47e97637231b0ad5d0caafa5
MartLopezF <23martinlopez@gmail.com>
2022-12-05 20:45:09 -0300

    models

--- Trabajo_Final/Tienda_Coder/models.py
+++ Trabajo_Final/Tienda_Coder/models.py
@@ -1,3 +1,31 @@
 from django.db import models
 
-# Create your models here.
+#Modelos a usar
+
+class Consolas(models.Model):
+    nombre = models.CharField(max_length=30)
+    marca = models.CharField(max_length=20)
+    precio = models.IntegerField()
+
+    def __str__(self):
+        return self.nombre
+
+class Juegos(models.Model):
+    nombre = models.CharField(max_length=50)
+    precio = models.IntegerField()
+
+    def __str__(self):
+        return self.nombre
+
+class Perifericos(models.Model):
+    nombre = models.CharField(max_length=30)
+    marca = models.CharField(max_length=20)
+    precio = models.IntegerField()
+
+    def __str__(self):
+        return self.nombre
+
+class Carrito(models.Model):
+    codigo_articulo = models.IntegerField()
+    cantidad = models.IntegerField()
+    suma_total = models.IntegerField()
